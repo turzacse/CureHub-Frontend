a5aee87a97b45eda00d94ae74adfd1a9d19eef07
turzacse <turzacse@gmail.com>
2024-02-22 12:16:10 +0600

    Basic react app setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      <h2 className='text-2xl font-bold m-10'>Its an demo project setup using react router and TailwindCSS</h2>
+    </div>
+  )
+}
+
+export default App
