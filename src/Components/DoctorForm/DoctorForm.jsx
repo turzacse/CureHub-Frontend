3e12d4b42a0925a76af89329b270d74e2d27a5b9
Fahim Montasir Turza <turzacse@gmail.com>
2024-06-12 23:15:51 +0600

    daisyUI setup

---
+++ src/Components/DoctorForm/DoctorForm.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import '../../App.css'
+
+const DoctorForm = ({ onClose, onSubmit }) => {
+    const [formData, setFormData] = useState({
+        name: '',
+        degree: '',
+        department: '',
+        designation: '',
+        doctors_code: '',
+        email: '',
+        phone: '',
+        offDay: '',
+        start_time: '',
+        end_time: '',
+        visit: '',
+        doctor_img: '',
+        joining_date: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(formData);
+        onClose();
+    };
+
+    return (
+        <div className="popup-form">
+            <h2 className='font-bold text-xl mb-5 text-center '>CureHub's Doctor</h2>
+            <form onSubmit={handleSubmit}>
+                <div className='flex justify-between gap-2'>
+                    <div className='flex-1'>
+                        {/* <label>Name:</label> */}
+                        <input type="text" placeholder='Name' name="name" value={formData.name} onChange={handleChange} required />
+                    </div>
+                    <div className='flex-1'>
+                        {/* <label>Degree:</label> */}
+                        <input type="text" placeholder='Degree' name="degree" value={formData.degree} onChange={handleChange} required />
+                    </div>
+                </div>
+                <div className='flex justify-between gap-2'>
+                    <div className='flex-1'>
+                        {/* <label>Department:</label> */}
+                        <input type="text" placeholder='Department' name="department" value={formData.department} onChange={handleChange} required />
+                    </div>
+                    <div className='flex-1'>
+                        {/* <label>Designation:</label> */}
+                        <input type="text" placeholder='Designation' name="designation" value={formData.designation} onChange={handleChange} required />
+                    </div>
+                    <div className='flex-1'>
+                        {/* <label>Doctor's Code:</label> */}
+                        <input type="text" placeholder={`Doctor's Code`} name="doctors_code" value={formData.doctors_code} onChange={handleChange} required />
+                    </div>
+                </div>
+
+                <div className='flex justify-between gap-2'>
+                    <div className='flex-1'>
+                        {/* <label>Email:</label> */}
+                        <input type="email" placeholder='Email' name="email" value={formData.email} onChange={handleChange} required />
+                    </div>
+                    <div className='flex-1'>
+                        {/* <label>Phone:</label> */}
+                        <input type="text" placeholder='Phone' name="phone" value={formData.phone} onChange={handleChange} required />
+                    </div>
+                    <div className='flex-1'>
+                        {/* <label>Off Day:</label> */}
+                        <input type="text" placeholder='Off Day' name="offDay" value={formData.offDay} onChange={handleChange} required />
+                    </div>
+                </div>
+                <div className='flex justify-between gap-2'>
+                    <div className='flex-1'>
+                        {/* <label>Start Time:</label> */}
+                        <input type="text" placeholder='Start Time' name="start_time" value={formData.start_time} onChange={handleChange} required />
+                    </div>
+                    <div className='flex-1'>
+                        {/* <label>End Time:</label> */}
+                        <input type="text" placeholder='End Time' name="end_time" value={formData.end_time} onChange={handleChange} required />
+                    </div>
+                </div>
+                <div className='flex justify-between gap-2'>
+                    <div className='flex-1'>
+                        {/* <label>Visit:</label> */}
+                        <input type="text" placeholder='Visit' name="visit" value={formData.visit} onChange={handleChange} required />
+                    </div>
+                    <div className='flex-1'>
+                        {/* <label>Doctor Image URL:</label> */}
+                        <input type="text" placeholder='Image' name="doctor_img" value={formData.doctor_img} onChange={handleChange} required />
+                    </div>
+                    <div className='flex-1'>
+                        {/* <label>Joining Date:</label> */}
+                        <input type="date" placeholder='Joining Date' name="joining_date" value={formData.joining_date} onChange={handleChange} required />
+                        <label className='font-normal text-sm'>Joining Date</label>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit">Register</button>
+                    <button type="button" onClick={onClose}>Close</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default DoctorForm;
--- src/Pages/Dashboard/Admin/DoctorsManagement/DoctorManagement.jsx
+++ src/Pages/Dashboard/Admin/DoctorsManagement/DoctorManagement.jsx
@@ -1,13 +1,107 @@
+// import React, { useEffect, useState } from 'react';
+// import Headline from '../../../../Components/Headline';
+// import { IoMdAddCircle } from 'react-icons/io';
+
+// const DoctorManagement = () => {
+//     const [doctors, setDoctors] = useState([]);
+//     const [departments, setDepartments] = useState([]);
+//     const [departmentDoctors, setDepartmentDoctors] = useState({});
+
+//     useEffect(() => {
+//         fetch('/Doctors.json')
+//             .then(res => res.json())
+//             .then(data => {
+//                 setDoctors(data);
+
+//                 // Create a mapping of departments to doctors
+//                 const departmentMap = data.reduce((acc, doctor) => {
+//                     const department = doctor.department;
+//                     if (!acc[department]) {
+//                         acc[department] = [];
+//                     }
+//                     acc[department].push(doctor);
+//                     return acc;
+//                 }, {});
+
+//                 setDepartmentDoctors(departmentMap);
+//             })
+//             .catch(error => {
+//                 console.error('There was a problem with the fetch operation:', error);
+//             });
+//     }, []);
+
+//     console.log(Object.keys(departmentDoctors).length);
+//     return (
+//         <div>
+//             <Headline headline='Doctors'></Headline>
+//             <div className='my-10 text-white flex gap-4'>
+//                 <h2 className='bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% py-3 rounded-xl font-medium text-lg md:text-xl w-[200px] text-center'>Total Doctor : {doctors?.length}</h2>
+
+//                 <h2 className='bg-gradient-to-r from-indigo-600 from-10% via-sky-600 via-30% to-emerald-600 to-90% py-3 rounded-xl font-medium text-lg md:text-xl w-[250px] text-center'>Total Departmnet : {Object.keys(departmentDoctors).length}</h2>
+
+//                 <div className='bg-gradient-to-r from-indigo-600 from-10% via-sky-600 via-30% to-emerald-600 to-90% py-3 rounded-xl font-medium text-lg md:text-xl w-[250px] text-center cursor-pointer flex justify-center items-center gap-2'>
+//                     <IoMdAddCircle /> 
+//                     <span>Register a Doctor</span> 
+//                 </div>
+//             </div>
+//             <div className='grid grid-cols-4 mb-10 rounded-xl bg-[#c5b984] p-4'>
+//                 {Object.keys(departmentDoctors).map(department => (
+//                     <div key={department}>
+//                         <h3 className='text-xl my-2 font-bold'>{department}</h3>
+//                         <ul>
+//                             {departmentDoctors[department].map(doctor => (
+//                                 <div key={doctor._id}>
+//                                 <li className='text-lg font-bold text-[#e05c3b]'>{doctor.name}</li>
+//                                 <li className='text-sm' >{doctor.degree.join(' ')}</li>
+//                                 </div>
+//                             ))}
+//                         </ul>
+//                     </div>
+//                 ))}
+//             </div>
+//             <div className="overflow-x-auto">
+//                 <table className="table bg-[#65acac] md:table-sm table-xs table-pin-rows table-pin-cols">
+//                     <thead>
+//                         <tr>
+//                             <th></th>
+//                             <td>Name</td>
+//                             <td>Department</td>
+//                             <td>Joining Date</td>
+//                             <td>Off Day</td>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         {
+//                             doctors?.map((data) => <tr key={data._id}>
+//                                 <th>{1}</th>
+//                                 <td>{data.name}</td>
+//                                 <td>{data.department}</td>
+//                                 <td>{data.joiningdate || ('12-12-12')}</td>
+//                                 <td>{data.offDay}</td>
+//                             </tr>)
+//                         }
+//                     </tbody>
+//                 </table>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default DoctorManagement;
+
+
 import React, { useEffect, useState } from 'react';
 import Headline from '../../../../Components/Headline';
 import { IoMdAddCircle } from 'react-icons/io';
+import DoctorForm from '../../../../Components/DoctorForm/DoctorForm';
+import '../../../../App.css'
+// import DoctorForm from './DoctorForm';
 
 const DoctorManagement = () => {
     const [doctors, setDoctors] = useState([]);
-
     const [departments, setDepartments] = useState([]);
-
     const [departmentDoctors, setDepartmentDoctors] = useState({});
+    const [isFormOpen, setIsFormOpen] = useState(false);
 
     useEffect(() => {
         fetch('/Doctors.json')
@@ -32,29 +126,44 @@ const DoctorManagement = () => {
             });
     }, []);
 
-    console.log(Object.keys(departmentDoctors).length);
+    const handleFormSubmit = (newDoctor) => {
+        const updatedDoctors = [...doctors, newDoctor];
+        setDoctors(updatedDoctors);
+
+        const departmentMap = updatedDoctors.reduce((acc, doctor) => {
+            const department = doctor.department;
+            if (!acc[department]) {
+                acc[department] = [];
+            }
+            acc[department].push(doctor);
+            return acc;
+        }, {});
+
+        setDepartmentDoctors(departmentMap);
+    };
+
     return (
         <div>
             <Headline headline='Doctors'></Headline>
-            <div className='my-10 text-white flex gap-4'>
+            <div className='my-10 text-white flex md:flex-row flex-col gap-4'>
                 <h2 className='bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% py-3 rounded-xl font-medium text-lg md:text-xl w-[200px] text-center'>Total Doctor : {doctors?.length}</h2>
 
                 <h2 className='bg-gradient-to-r from-indigo-600 from-10% via-sky-600 via-30% to-emerald-600 to-90% py-3 rounded-xl font-medium text-lg md:text-xl w-[250px] text-center'>Total Departmnet : {Object.keys(departmentDoctors).length}</h2>
 
-                <div className='bg-gradient-to-r from-indigo-600 from-10% via-sky-600 via-30% to-emerald-600 to-90% py-3 rounded-xl font-medium text-lg md:text-xl w-[250px] text-center cursor-pointer flex justify-center items-center gap-2'>
+                <div className='bg-gradient-to-r from-indigo-600 from-10% via-sky-600 via-30% to-emerald-600 to-90% py-3 rounded-xl font-medium text-lg md:text-xl w-[250px] text-center cursor-pointer flex justify-center items-center gap-2' onClick={() => setIsFormOpen(true)}>
                     <IoMdAddCircle /> 
                     <span>Register a Doctor</span> 
                 </div>
             </div>
-            <div className='grid grid-cols-4 mb-10 rounded-xl bg-[#c5b984] p-4'>
+            <div className='grid md:grid-cols-4 grid-cols-2 mb-10 rounded-xl bg-[#c5b984] p-4'>
                 {Object.keys(departmentDoctors).map(department => (
                     <div key={department}>
                         <h3 className='text-xl my-2 font-bold'>{department}</h3>
                         <ul>
                             {departmentDoctors[department].map(doctor => (
                                 <div key={doctor._id}>
-                                <li className='text-lg font-bold text-[#e05c3b]'>{doctor.name}</li>
-                                <li className='text-sm' >{doctor.degree.join(' ')}</li>
+                                    <li className='text-lg font-bold text-[#e05c3b]'>{doctor.name}</li>
+                                    <li className='text-sm'>{doctor.degree.join(' ')}</li>
                                 </div>
                             ))}
                         </ul>
@@ -73,18 +182,23 @@ const DoctorManagement = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            doctors?.map((data) => <tr key={data._id}>
-                                <th>{1}</th>
+                        {doctors?.map((data, index) => (
+                            <tr key={data._id}>
+                                <th>{index + 1}</th>
                                 <td>{data.name}</td>
                                 <td>{data.department}</td>
-                                <td>{data.joiningdate || ('12-12-12')}</td>
+                                <td>{data.joining_date || '12-12-12'}</td>
                                 <td>{data.offDay}</td>
-                            </tr>)
-                        }
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
+            {isFormOpen && (
+                <div className="popup-overlay">
+                    <DoctorForm onClose={() => setIsFormOpen(false)} onSubmit={handleFormSubmit} />
+                </div>
+            )}
         </div>
     );
 };
