cc61de851c45bad93a92fc9a6e5c4d1406b90dfa
Fahim Montasir Turza <turzacse@gmail.com>
2024-09-02 23:26:43 +0600

    Privecy Policy and Term of Use

---
+++ src/Pages/PrivecyPolicy/PrivecyPolicy.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Heading from '../../Components/PageHeading/Heading';
+
+const PrivecyPolicy = () => {
+    return (
+        <div>
+            <Heading title='Privecy Policy'/>
+        </div>
+    );
+};
+
+export default PrivecyPolicy;
---
+++ src/Pages/TermOfService/TermOfService.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Heading from '../../Components/PageHeading/Heading';
+
+const TermOfService = () => {
+    return (
+        <div>
+            <Heading title='Term Of Service' />
+        </div>
+    );
+};
+
+export default TermOfService;
